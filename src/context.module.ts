f861166c51ec7faabaa4b274de602d49508f05d5
g.demarco <devnull@localhost>
2017-12-01 15:55:05 +0100

    aggiunta gestione del riutilizzo delle rotte

--- src/context.module.ts
+++ src/context.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from "@angular/common";
 import {ContextModuleConfig} from "./context-module-config";
 import {SharedData} from "./shared-data";
 import {OdataContextFactory} from "./odata-context-factory";
+import {RouteReuseStrategy} from "@angular/router";
+import {CustomReuseStrategy} from "./Routes/custom-reuse-strategy";
 
 
 
@@ -17,7 +19,12 @@ export class ContextModule {
   static forRoot(config: ContextModuleConfig): ModuleWithProviders {
     return {
       ngModule: ContextModule,
-      providers: [OdataContextFactory, {provide: 'config', useValue: config}, SharedData]
+      providers: [
+          OdataContextFactory,
+          {provide: 'config', useValue: config},
+          SharedData,
+          {provide: RouteReuseStrategy, useClass: CustomReuseStrategy}
+          ]
     }
   }
 }
