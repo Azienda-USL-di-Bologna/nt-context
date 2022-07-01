1c80638443780fc05db7ac03082497d52a80c72b
D'Onza Michele <m.donza@nextsw.it>
2022-07-01 12:23:03 +0200

    first commit

---
+++ src/odata-context-factory.ts
@@ -0,0 +1,29 @@
+import {Inject, Injectable} from "@angular/core";
+import {ContextModuleConfig} from "./context-module-config";
+import {OdataContextEntitiesDefinition} from "./odata-context-entities-definition";
+import {OdataContextViewsDefinition} from "./odata-context-views-definition";
+import {OdataContextFunctionsImportDefinition} from "./odata-context-functions-import-definition";
+
+@Injectable()
+export class OdataContextFactory {
+    constructor(@Inject('config') private config: ContextModuleConfig) {
+    }
+
+    public buildOdataContextEntitiesDefinition(): OdataContextEntitiesDefinition {
+        const odataContextEntitiesDefinition: OdataContextEntitiesDefinition = new OdataContextEntitiesDefinition();
+        odataContextEntitiesDefinition.buildOdataContext(this.config);
+        return odataContextEntitiesDefinition;
+    }
+
+    public buildOdataContextViewsDefinition(): OdataContextViewsDefinition {
+        const odataContextViewsDefinition: OdataContextViewsDefinition = new OdataContextViewsDefinition();
+        odataContextViewsDefinition.buildOdataContext(this.config);
+        return odataContextViewsDefinition;
+    }
+
+    public buildOdataFunctionsImportDefinition(): OdataContextFunctionsImportDefinition {
+        const odataContextFunctionsImportDefinition: OdataContextFunctionsImportDefinition = new OdataContextFunctionsImportDefinition();
+        odataContextFunctionsImportDefinition.buildOdataContext(this.config);
+        return odataContextFunctionsImportDefinition;
+    }
+}
